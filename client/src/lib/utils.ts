07afde37251ffeaf4c64871dc95fbbf18c37c506
umarthalha <coaefriend@gmail.com>
2026-06-11 12:46:45 +0530

    Add files via upload

---
+++ client/src/lib/utils.ts
@@ -0,0 +1,20 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+    maximumFractionDigits: 2,
+  }).format(value);
+}
+
+export function formatIndianNumber(value: number): string {
+  return new Intl.NumberFormat('en-IN', {
+    maximumFractionDigits: 2,
+  }).format(value);
+}
