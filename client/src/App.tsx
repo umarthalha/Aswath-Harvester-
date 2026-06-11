1dff328e882a3e3810eb639efe9b2099fbfcc7c9
umarthalha <coaefriend@gmail.com>
2026-06-11 14:47:10 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@
  */
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Layout } from './components/layout/Layout';
+import Layout from "./components/layout/index"
 import { Dashboard } from './pages/Dashboard';
 import { Universe } from './pages/Universe';
 import { Screener } from './pages/Screener';
